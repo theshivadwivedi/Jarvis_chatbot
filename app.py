3e23e10e19993bdf9db22d36627d0ff52daf3eb0
Shiva Dwivedi <dwivedishiva2006@gmail.com>
2025-11-17 22:49:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import uvicorn
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from inference_engine import decode_greedy  # your model
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Serve static folder
 app.mount("/static", StaticFiles(directory="static"), name="static")
